31cd9864a5b1dec8a1482c7e9a1a1b2d432bb5a8
lakshmi <lakshmicd2003@gmail.com>
2024-01-04 13:19:17 +0530

    login page created

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from student.views import homepage
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',homepage,name="index.html")
 ]
--- student/views.py
+++ student/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from student.views import *
 
 # Create your views here.
+def homepage(request):
+    return render(request, 'index.html')
+
+def login(request)
